ff5cdbc8781c455994ac8ae5679e921bb587f359
kuroko <kurokog2a@gmail.com>
2020-01-31 00:23:56 -0300

    change

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $public = ""; //Recaptcha Public Key
 $secret = ""; // Recaptcha Privat Key
 
-$ts3_host = "localhost";
+$ts3_host = "191.234.169.147";
 $ts3_q_port = "10011";
 $ts3_s_port = "9987";
 $ts3_username = "serveradmin";
@@ -14,7 +14,7 @@ $chadmin_group_id = 5; // Channel Admin Group ID Default 5
 
 $channel_description = "";
 $channel_topic = "CHADD | WebUI @ ".date("H:i:s")." - ".date("d.m.Y");
-$server_conn_url = "ts3server://myip:port";
+$server_conn_url = "ts3server://191.234.169.147:9987";
 $imprint_url = "";
 
 //BADWORDS will be replaced with XXXX
