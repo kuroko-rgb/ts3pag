8a4a2099daeeaeb0692df33f152549f561e02e20
kuroko <kurokog2a@gmail.com>
2020-01-31 12:31:49 -0300

    chnab

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?PHP
-$public = ""; //Recaptcha Public Key
-$secret = ""; // Recaptcha Privat Key
+$public = "6LeTnNQUAAAAAEwgQxmAP4Z4qIpq8yrm040N4xLg"; //Recaptcha Public Key
+$secret = "6LeTnNQUAAAAAO5BPGpDathCLl89u9Od5q1XSERO"; // Recaptcha Privat Key
 
 $ts3_host = "191.234.169.147";
 $ts3_q_port = "10011";
@@ -14,8 +14,8 @@ $chadmin_group_id = 5; // Channel Admin Group ID Default 5
 
 $channel_description = "";
 $channel_topic = "CHADD | WebUI @ ".date("H:i:s")." - ".date("d.m.Y");
-$server_conn_url = "ts3server://191.234.169.147:9987";
-$imprint_url = "";
+$server_conn_url = "ts3server://:9987";
+$imprint_url = "https://loscalvos.azurewebsites.net/";
 
 //BADWORDS will be replaced with XXXX
 $badwords = array("example", "example");
